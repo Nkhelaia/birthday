967146db154ae212949851dacd8d360208cd9308
Nanuka Khelaia <98689876+Nkhelaia@users.noreply.github.com>
2022-03-04 20:41:06 +0400

    happy birthday
    happy birth day

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('logo').addEventListener('click', function(){
+    document.getElementById('card').style.display = 'none';
+    document.getElementById('happy1').style.display = 'block';
+})
